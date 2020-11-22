df1aa48ae802e4358b484bcd385b4d2b901f4fa4
Theofilus Marcel Irawan Sutomo <50488473+MarcelIrawan@users.noreply.github.com>
2020-11-22 22:10:09 +0700

    re-routing home page and change course layout

--- Startup.cs
+++ Startup.cs
@@ -59,7 +59,7 @@ namespace BagiTugas
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Courses}/{action=Index}/{id?}");
             });
         }
     }
